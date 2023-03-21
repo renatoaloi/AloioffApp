d07aa8d6069f4971bc9a373c4c8d0b27e3284c90
Renato Aloi <renato.aloi@gmail.com>
2023-03-21 14:23:32 -0300

    initial tab bar commit

--- src/stacks/MainTab.js
+++ src/stacks/MainTab.js
@@ -2,15 +2,15 @@ import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import CustomTabBar from '../components/CustomTabBar';
 import Home from '../screens/Home';
-import Historic from '../screens/Historic';
 import Profile from '../screens/Profile';
+import NewDevice from '../screens/NewDevice';
 
 const Tab = createBottomTabNavigator();
 
 export default () => (
   <Tab.Navigator tabBar={props => <CustomTabBar {...props} />}>
     <Tab.Screen name="Home" component={Home} />
-    <Tab.Screen name="Historic" component={Historic} />
+    <Tab.Screen name="New Device" component={NewDevice} />
     <Tab.Screen name="Profile" component={Profile} />
   </Tab.Navigator>
 );
