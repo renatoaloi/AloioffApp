22c9f8bb1a5a965e03ee8027ee655a8ad8d1cbf7
Renato Aloi <renato.aloi@gmail.com>
2023-03-21 14:23:52 -0300

    limpeza

--- src/stacks/MainTab.js
+++ src/stacks/MainTab.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import CustomTabBar from '../components/CustomTabBar';
 import Home from '../screens/Home';
 import Profile from '../screens/Profile';
 import NewDevice from '../screens/NewDevice';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
 export default () => (
-  <Tab.Navigator tabBar={props => <CustomTabBar {...props} />}>
+  <Tab.Navigator
+    screenOptions={({route}) => ({
+      tabBarIcon: ({focused, color, size}) => {
+        let iconName;
+
+        if (route.name === 'Home') {
+          iconName = focused
+            ? 'ios-information-circle'
+            : 'ios-information-circle-outline';
+        } else if (route.name === 'Settings') {
+          iconName = focused ? 'ios-list' : 'ios-list-outline';
+        }
+
+        // You can return any component that you like here!
+        return <Ionicons name={iconName} size={size} color={color} />;
+      },
+      tabBarActiveTintColor: 'tomato',
+      tabBarInactiveTintColor: 'gray',
+    })}>
     <Tab.Screen name="Home" component={Home} />
     <Tab.Screen name="New Device" component={NewDevice} />
     <Tab.Screen name="Profile" component={Profile} />
