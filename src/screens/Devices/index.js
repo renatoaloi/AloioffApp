425d29b2cf3e8355b98500cd9e51c46e79e3eba5
Roberto Silvestre <roberto@aloitech.com.br>
2023-03-23 14:07:02 -0300

    adcionando lista

--- src/screens/Devices/index.js
+++ src/screens/Devices/index.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Container, Header, H1, PrimaryText} from './styles';
-import {ScrollView, StatusBar, Text, View} from 'react-native';
+import {
+  Alert,
+  ScrollView,
+  StatusBar,
+  Text,
+  TouchableOpacity,
+  View,
+  FlatList,
+  StyleSheet,
+} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import db from '../../../db.json';
 import * as Animatable from 'react-native-animatable';
@@ -12,6 +21,24 @@ import AntDesign from 'react-native-vector-icons/AntDesign';
 
 export default () => {
   const navigation = useNavigation();
+  const [count, setCount] = useState(0);
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      paddingTop: 22,
+    },
+    item: {
+      padding: 10,
+      height: 44,
+    },
+  });
+
+  // function addButtonPress() {
+  //   setCount(prevCount => prevCount + 1);
+  // }
+
+  const addButtonPress = () => setCount(count + 1);
 
   return (
     <Container>
@@ -37,20 +64,19 @@ export default () => {
           }}>
           DISPOSITIVOS
         </Text>
-        <AntDesign
+        <TouchableOpacity
+          onPress={addButtonPress}
           style={{
             paddingTop: 22,
-            paddingRight: 20,
+            paddingRight: 22,
             flex: 0.2,
-            textAlign: 'right',
-          }}
-          name="pluscircleo"
-          size={26}
-          color="tomato"
-        />
+            alignItems: 'flex-end',
+          }}>
+          <AntDesign name="pluscircleo" size={26} color="tomato" />
+        </TouchableOpacity>
       </View>
-      <ScrollView
-        style={{flex: 0.5, backgroundColor: 'black', marginBottom: 100}}>
+
+      <View style={{flex: 0.8, marginBottom: 5, backgroundColor: 'black'}}>
         <View style={{flexDirection: 'row'}}>
           <Text style={{marginTop: 20}}>Novo Dispositivo</Text>
           <MaterialIcons
@@ -61,33 +87,34 @@ export default () => {
           />
         </View>
 
-        <Text style={{marginTop: 20}}>Texto3</Text>
-        <Text style={{marginTop: 20}}>Texto3</Text>
-        <Text style={{marginTop: 20}}>Texto3</Text>
-        <Text style={{marginTop: 20}}>Texto3</Text>
-        <Text style={{marginTop: 20}}>Texto3</Text>
-        <Text style={{marginTop: 20}}>Texto3</Text>
-        <Text style={{marginTop: 20}}>Texto3</Text>
-        <Text style={{marginTop: 20}}>Texto3</Text>
-        <Text style={{marginTop: 20}}>Texto3</Text>
-        <Text style={{marginTop: 20}}>Texto3</Text>
-        <Text style={{marginTop: 20}}>Texto3</Text>
-        <Text style={{marginTop: 20}}>Texto3</Text>
-        <Text style={{marginTop: 20}}>Texto3</Text>
-        <Text style={{marginTop: 20}}>Texto3</Text>
-        <Text style={{marginTop: 20}}>Texto3</Text>
-        <Text style={{marginTop: 20}}>Texto3</Text>
-        <Text style={{marginTop: 20}}>Texto3</Text>
-        <Text style={{marginTop: 20}}>Texto3</Text>
-        <Text style={{marginTop: 20}}>Texto3</Text>
-        <Text style={{marginTop: 20}}>Texto3</Text>
-        <Text style={{marginTop: 20}}>Texto3</Text>
-        <Text style={{marginTop: 20}}>Texto3</Text>
-        <Text style={{marginTop: 20}}>Texto3</Text>
-        <Text style={{marginTop: 20}}>Texto3</Text>
-        <Text style={{marginTop: 20}}>Texto3</Text>
-        <Text style={{marginTop: 20}}>TextoFinal</Text>
-      </ScrollView>
+        <Text style={{marginTop: 20}}>Count: {count}</Text>
+
+        <FlatList
+          data={[
+            {key: 'Devin'},
+            {key: 'Dan'},
+            {key: 'Dominic'},
+            {key: 'Jackson'},
+            {key: 'James'},
+            {key: 'Joel'},
+            {key: 'John'},
+            {key: 'Jillian'},
+            {key: 'Jimmy'},
+            {key: 'Julie'},
+            {key: 'Roberto'},
+            {key: 'Lucas'},
+            {key: 'Leonardo'},
+            {key: 'Thiago'},
+            {key: 'João'},
+            {key: 'José'},
+            {key: 'Vilma'},
+            {key: 'Gilberto'},
+            {key: 'Maria'},
+            {key: 'Marcelo'},
+          ]}
+          renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+        />
+      </View>
     </Container>
   );
 };
