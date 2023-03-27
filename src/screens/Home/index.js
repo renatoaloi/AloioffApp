3a7cc7300a8a90610762ad2b32890545cc07d86c
Roberto Silvestre <roberto@aloitech.com.br>
2023-03-27 14:43:35 -0300

    correção bug header

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -26,32 +26,36 @@ export default () => {
         backgroundColor={db.theme.colors.statuBar}
         barStyle="light-content"
       />
-      <Header>
-        <View
-          style={{
-            flex: 0.1,
-            backgroundColor: 'black',
-            flexDirection: 'row',
-          }}>
-          <View style={{flex: 0.2}}></View>
-          <View style={{overflow: 'visible'}}>
-            <Image
-              style={{width: 297, height: 144, marginLeft: 20}}
-              source={logoHome}
-            />
-          </View>
-          <TouchableOpacity
-            onPress={addButtonPress}
-            style={{
-              paddingTop: 22,
-              paddingRight: 22,
-              flex: 0.2,
-              alignItems: 'flex-end',
-            }}>
-            <AntDesign name="pluscircleo" size={26} color="tomato" />
-          </TouchableOpacity>
+      <View
+        style={{
+          flex: 0.1,
+          backgroundColor: 'black',
+          flexDirection: 'row',
+        }}>
+        <View style={{flex: 0.2}}>
+          <Text>Ola Mundo</Text>
         </View>
-      </Header>
+        <View style={{flex: 0.6}}>
+          <Image
+            style={{
+              width: 60,
+              height: 30,
+              marginLeft: 20,
+            }}
+            source={logoHome}
+          />
+        </View>
+        <TouchableOpacity
+          onPress={addButtonPress}
+          style={{
+            paddingTop: 22,
+            paddingRight: 22,
+            flex: 0.2,
+            alignItems: 'flex-end',
+          }}>
+          <AntDesign name="pluscircleo" size={26} color="tomato" />
+        </TouchableOpacity>
+      </View>
 
       <ScrollView>
         <Text>AloiApp (AloiOscar?) AloiEco</Text>
