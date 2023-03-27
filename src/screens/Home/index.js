64be4a39dfb88cb329fc8a76cdc08049214a1b2f
Roberto Silvestre <roberto@aloitech.com.br>
2023-03-27 14:35:57 -0300

    ajuste header da pagina home

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import {Container, Header, H1, PrimaryText} from './styles';
-import {ScrollView, StatusBar, Text} from 'react-native';
+import {
+  ScrollView,
+  StatusBar,
+  Text,
+  View,
+  TouchableOpacity,
+  Alert,
+  Image,
+} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import db from '../../../db.json';
 import * as Animatable from 'react-native-animatable';
 import styleGlobal from '../../styles/global';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import logoHome from '../../imgs/logoHome';
 
 export default () => {
   const navigation = useNavigation();
+  const addButtonPress = () => Alert.alert('Ajuda');
 
   return (
     <Container>
@@ -15,6 +26,33 @@ export default () => {
         backgroundColor={db.theme.colors.statuBar}
         barStyle="light-content"
       />
+      <Header>
+        <View
+          style={{
+            flex: 0.1,
+            backgroundColor: 'black',
+            flexDirection: 'row',
+          }}>
+          <View style={{flex: 0.2}}></View>
+          <View style={{overflow: 'visible'}}>
+            <Image
+              style={{width: 297, height: 144, marginLeft: 20}}
+              source={logoHome}
+            />
+          </View>
+          <TouchableOpacity
+            onPress={addButtonPress}
+            style={{
+              paddingTop: 22,
+              paddingRight: 22,
+              flex: 0.2,
+              alignItems: 'flex-end',
+            }}>
+            <AntDesign name="pluscircleo" size={26} color="tomato" />
+          </TouchableOpacity>
+        </View>
+      </Header>
+
       <ScrollView>
         <Text>AloiApp (AloiOscar?) AloiEco</Text>
         <Text>Aplicativo para a tomada inteligente Aloioff!</Text>
