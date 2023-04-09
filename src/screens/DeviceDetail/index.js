ba508d9b16976c0fbd902d58f74db7f7499f2d44
Renato Aloi <renato.aloi@gmail.com>
2023-04-09 11:04:20 -0300

    device detail icon

--- src/screens/DeviceDetail/index.js
+++ src/screens/DeviceDetail/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import {Text, View, StatusBar} from 'react-native';
 import db from '../../../db.json';
-import {PrimaryText, PrimaryView, Container} from './styles';
+import {PrimaryTitle, PrimaryText, PrimaryView, Container} from './styles';
 import {useNavigation} from '@react-navigation/native';
 import DeviceDetailHeader from '../../components/DeviceDetailHeader';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 export default ({route, navigation}) => {
   const {ip, id, typeId, typeDescription, deviceName} = route.params;
   const addButtonPress = () => {
-    console.log('passei aqui');
     navigation.goBack();
   };
 
@@ -20,11 +20,39 @@ export default ({route, navigation}) => {
       />
       <DeviceDetailHeader addButtonPress={addButtonPress} />
       <PrimaryView>
-        <PrimaryText>ip: {ip}</PrimaryText>
-        <PrimaryText>Id: {id}</PrimaryText>
-        <PrimaryText>TypeId: {typeId}</PrimaryText>
-        <PrimaryText>Type Description: {typeDescription}</PrimaryText>
-        <PrimaryText>Device Name: {deviceName}</PrimaryText>
+        <View style={{flex: 1, flexDirection: 'row'}}>
+          <View style={{flex: 0.6}}>
+            <PrimaryTitle>IP: </PrimaryTitle>
+            <PrimaryText>{ip}</PrimaryText>
+            <PrimaryTitle>UserID: </PrimaryTitle>
+            <PrimaryText>{id}</PrimaryText>
+            <PrimaryTitle>Modo de Operação: </PrimaryTitle>
+            <PrimaryText>{typeDescription}</PrimaryText>
+            <PrimaryTitle>Nome do Dispositivo: </PrimaryTitle>
+            <PrimaryText>{deviceName}</PrimaryText>
+          </View>
+          <View
+            style={{
+              flex: 0.4,
+              alignItems: 'center',
+            }}>
+            {typeId === 1 ? (
+              <Ionicons name="logo-amazon" size={120} color="white" />
+            ) : typeId === 2 ? (
+              <Ionicons name="logo-google" size={120} color="white" />
+            ) : typeId === 3 ? (
+              <Ionicons name="home" size={120} color="white" />
+            ) : typeId === 4 ? (
+              <Ionicons name="logo-nodejs" size={120} color="white" />
+            ) : (
+              <Ionicons
+                name="ios-help-circle-outline"
+                size={120}
+                color="white"
+              />
+            )}
+          </View>
+        </View>
       </PrimaryView>
     </Container>
   );
--- src/screens/DeviceDetail/styles.js
+++ src/screens/DeviceDetail/styles.js
@@ -3,7 +3,16 @@ import db from '../../../db.json';
 
 export const PrimaryText = styled.Text`
   font-size: 20px;
-  color: ${db.theme.colors.primary};
+  margin-left: 10px;
+  margin-top: 4px;
+  color: ${db.theme.colors.text};
+`;
+
+export const PrimaryTitle = styled.Text`
+  font-size: 20px;
+  margin-left: 10px;
+  margin-top: 10px;
+  color: ${db.theme.colors.success};
 `;
 
 export const PrimaryView = styled.View`
